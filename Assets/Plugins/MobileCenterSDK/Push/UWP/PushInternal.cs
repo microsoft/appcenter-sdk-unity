5cdeeceff5cb7283976d4288395409b4339b4da6
Zander Chocron <alchocro@microsoft.com>
2017-07-13 11:36:35 -0700

    Fix indentation

--- Assets/Plugins/MobileCenterSDK/Push/UWP/PushInternal.cs
+++ Assets/Plugins/MobileCenterSDK/Push/UWP/PushInternal.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 //
 // Licensed under the MIT license.
 
@@ -12,10 +12,10 @@ namespace Microsoft.Azure.Mobile.Push.Unity.Internal
 
     class PushInternal
     {
-		public static Type mobile_center_unity_push_get_type()
+        public static Type mobile_center_unity_push_get_type()
         {
             return typeof(UWPPush);
-		}
+        }
 
         public static void mobile_center_unity_push_set_enabled(bool isEnabled)
         {
@@ -24,8 +24,8 @@ namespace Microsoft.Azure.Mobile.Push.Unity.Internal
 
         public static bool mobile_center_unity_push_is_enabled()
         {
-			return UWPPush.Enabled;
-		}
+            return UWPPush.Enabled;
+        }
 
         public static void mobile_center_unity_push_check_launched_from_notification(LaunchActivatedEventArgs e)
         {
@@ -35,6 +35,6 @@ namespace Microsoft.Azure.Mobile.Push.Unity.Internal
         public static void mobile_center_unity_push_enable_firebase_analytics()
         {
         }
-	}
+    }
 }
 #endif
