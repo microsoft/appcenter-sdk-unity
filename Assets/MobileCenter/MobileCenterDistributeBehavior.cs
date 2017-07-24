766fa440cb7a039e3a3ef65d5dea2288e1ebd99f
Alexander Chocron <alchocro@microsoft.com>
2017-07-24 14:39:01 -0700

    Reformat files

--- Assets/MobileCenter/MobileCenterDistributeBehavior.cs
+++ Assets/MobileCenter/MobileCenterDistributeBehavior.cs
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 //
 // Licensed under the MIT license.
 
@@ -8,14 +8,14 @@ using Microsoft.Azure.Mobile.Unity.Distribute;
 
 public class MobileCenterDistributeBehavior : MonoBehaviour
 {
-	[Header("Basic Setup")]
-	public bool UseDistribute = true;
+    [Header("Basic Setup")]
+    public bool UseDistribute = true;
 
-	public void Awake()
+    public void Awake()
     {
         if (UseDistribute)
         {
-			MobileCenter.Start(typeof(Distribute));
-		}
+            MobileCenter.Start(typeof(Distribute));
+        }
     }
 }
