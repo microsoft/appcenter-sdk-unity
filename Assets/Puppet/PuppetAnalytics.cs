05dc70ecf3bbc4829203518d6b5123676e155ac9
Ivan Matkov <v-ivmatk@microsoft.com>
2017-07-25 17:34:41 +0300

    Add puppet scene and basic behavior

---
+++ Assets/Puppet/PuppetAnalytics.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Mobile.Unity.Analytics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuppetAnalytics : MonoBehaviour
+{
+    public Toggle Enabled;
+    public InputField EventName;
+
+    void OnEnable()
+    {
+        Enabled.isOn = Analytics.Enabled;
+    }
+
+    public void SetEnabled(bool enabled)
+    {
+        Analytics.Enabled = enabled;
+        Enabled.isOn = Analytics.Enabled;
+    }
+
+    public void TrackEvent()
+    {
+        Analytics.TrackEvent(EventName.text);
+    }
+}
---
+++ Assets/Puppet/PuppetCrashes.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.Mobile.Unity.Crashes;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuppetCrashes : MonoBehaviour
+{
+    public Toggle Enabled;
+
+    void OnEnable()
+    {
+        Enabled.isOn = Crashes.Enabled;
+    }
+
+    public void SetEnabled(bool enabled)
+    {
+        Crashes.Enabled = enabled;
+        Enabled.isOn = Crashes.Enabled;
+    }
+    public void TestCrash()
+    {
+        //Crashes.GenerateTestCrash();
+    }
+
+    public void DivideByZero()
+    {
+        Debug.Log(42 / int.Parse("0"));
+    }
+
+    public void NullReferenceException()
+    {
+        string str = null;
+        Debug.Log(str.Length);
+    }
+}
