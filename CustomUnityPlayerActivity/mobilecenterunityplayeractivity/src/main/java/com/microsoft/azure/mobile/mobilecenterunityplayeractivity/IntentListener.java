6f6aaef8096340c060795f6ab14e76fb6f3bc238
Alexander Chocron <alchocro@microsoft.com>
2017-08-03 11:14:22 -0700

    Remove custom activity and manifest

--- CustomUnityPlayerActivity/mobilecenterunityplayeractivity/src/main/java/com/microsoft/azure/mobile/mobilecenterunityplayeractivity/IntentListener.java
+++
@@ -1,8 +0,0 @@
-package com.microsoft.azure.mobile.mobilecenterunityplayeractivity;
-
-import android.app.Activity;
-import android.content.Intent;
-
-public interface IntentListener {
-    void onNewIntent(Activity activity, Intent intent);
-}
