cb7189266ce7568d7ef5a8d9c0e3907d78439711
Alexander Chocron <alchocro@microsoft.com>
2017-08-15 11:20:56 -0700

    Dont use nuget to add json.net

--- Assets/MobileCenter/Editor/MobileCenterPostBuild.cs
+++ Assets/MobileCenter/Editor/MobileCenterPostBuild.cs
@@ -83,7 +83,6 @@ public class MobileCenterPostBuild
         var jsonString = File.ReadAllText(projectJsonPath);
         jsonString = AddDependencyToProjectJson(jsonString, "Microsoft.NETCore.UniversalWindowsPlatform", "5.3.3");
         jsonString = AddDependencyToProjectJson(jsonString, "sqlite-net-pcl", "1.3.3");
-        jsonString = AddDependencyToProjectJson(jsonString, "Newtonsoft.Json", "10.0.2");
         File.WriteAllText(projectJsonPath, jsonString);
     }
 
