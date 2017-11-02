9858ef8f8cbc7be9012add5d01b27307a3491043
Ivan Matkov <v-ivmatk@microsoft.com>
2017-11-02 12:01:00 +0300

    Remove copy of SDK from demo app

--- AppCenterDemoApp/Assets/AppCenter/Plugins/iOS/Analytics/AnalyticsUnity.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-//
-// Licensed under the MIT license.
-
-extern "C" void* appcenter_unity_analytics_get_type();
-extern "C" void appcenter_unity_analytics_track_event(char* eventName);
-extern "C" void appcenter_unity_analytics_track_event_with_properties(char* eventName, char** keys, char** values, int count);
-extern "C" void appcenter_unity_analytics_set_enabled(bool isEnabled);
-extern "C" bool appcenter_unity_analytics_is_enabled();
