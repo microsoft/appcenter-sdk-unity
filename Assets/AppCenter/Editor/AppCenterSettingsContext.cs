86dc3d9702bdca075621f4a7562729b9a3007f78
Alexander Chocron <alchocro@microsoft.com>
2018-01-08 10:45:52 -0800

    Remove unnecessary lock in settings context

--- Assets/AppCenter/Editor/AppCenterSettingsContext.cs
+++ Assets/AppCenter/Editor/AppCenterSettingsContext.cs
@@ -3,24 +3,20 @@ using UnityEditor;
 
 public class AppCenterSettingsContext : ScriptableObject
 {
-    private static readonly object SettingsLock = new object();
     private const string SettingsPath = "Assets/AppCenter/AppCenterSettings.asset";
     public static AppCenterSettings SettingsInstance
     {
         get
         {
-            lock (SettingsLock)
+            // No need to lock because this can only be accessed from the main thread
+            var instance = AssetDatabase.LoadAssetAtPath<AppCenterSettings>(SettingsPath);
+            if (instance == null)
             {
-                var instance = AssetDatabase.LoadAssetAtPath<AppCenterSettings>(SettingsPath);
-                if (instance == null)
-                {
-                    instance = CreateInstance<AppCenterSettings>();
-                    AssetDatabase.CreateAsset(instance, SettingsPath);
-                    AssetDatabase.SaveAssets();
-                }
-                return instance;
+                instance = CreateInstance<AppCenterSettings>();
+                AssetDatabase.CreateAsset(instance, SettingsPath);
+                AssetDatabase.SaveAssets();
             }
+            return instance;
         }
     }
-
 }
