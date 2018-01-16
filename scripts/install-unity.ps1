85088adc165d4b6d96ac80ff9b83f2b881ccd7a4
Alexander Chocron <alchocro@microsoft.com>
2018-01-16 11:07:01 -0800

    Add print statement to install script

--- scripts/install-unity.ps1
+++ scripts/install-unity.ps1
@@ -6,6 +6,8 @@ Param(
     [string]$InstallPath
 )
 
+Write-Host "Installing Unity Editor and tools to directory " + $(InstallPath)
+
 # Download links for Unity 2017.3.0f3, editor and support
 $WindowsUnityEditorUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/Windows64EditorInstaller/UnitySetup64-2017.3.0f3.exe?_ga=2.69975952.1053985475.1515441432-1315763065.1501872160"
 $WSADotNetSupportWindowsUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/TargetSupportInstaller/UnitySetup-Metro-Support-for-Editor-2017.3.0f3.exe?_ga=2.69975952.1053985475.1515441432-1315763065.1501872160"
@@ -18,9 +20,16 @@ Write-Host "Downloading Unity Editor..."
 Write-Host "Installing Unity Editor..."
 ./UnitySetup64.exe /S /D=$InstallPath
 
-#install support (.net)
+# Install support tools (.NET)
 $path = (Get-Location).Path + "\UnityNetSupport.exe";
 Write-Host "Downloading Unity .NET support..."
 (New-Object System.Net.WebClient).DownloadFile($WSADotNetSupportWindowsUrl, $path) 
 Write-Host "Installing Unity .NET support..."
 ./UnityNetSupport.exe /S /D=$InstallPath
+
+#install support tools (IL2CPP)
+$path = (Get-Location).Path + "\UnityIl2CppSupport.exe";
+Write-Host "Downloading Unity IL2CPP support..."
+(New-Object System.Net.WebClient).DownloadFile($WSAIl2CppSupportWindowsUrl, $path) 
+Write-Host "Installing Unity IL2CPP support..."
+./UnityIl2CppSupport.exe /S /D=$InstallPath
