adbadabb53942018879f86aa2d0c662525025a83
Alexander Chocron <alchocro@microsoft.com>
2018-01-16 10:45:34 -0800

    Make install path an argument

--- scripts/install-unity.ps1
+++ scripts/install-unity.ps1
@@ -1,7 +1,15 @@
-$WindowsUnityEditorUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/Windows64EditorInstaller/UnitySetup64-2017.3.0f3.exe?_ga=2.69975952.1053985475.1515441432-1315763065.1501872160"
-$WSADotNetSupportWindowsUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/TargetSupportInstaller/UnitySetup-Metro-Support-for-Editor-2017.3.0f3.exe?_ga=2.69975952.1053985475.1515441432-1315763065.1501872160";
+# Installs Unity and required components for WSA .NET and IL2CPP builds
 
-$InstallPath = (Get-Location).Path + "\Unity"
+[CmdletBinding()]
+Param(
+    # Path to install unity and tools to
+    [switch]$InstallPath
+)
+
+# Download links for Unity 2017.3.0f3, editor and support
+$WindowsUnityEditorUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/Windows64EditorInstaller/UnitySetup64-2017.3.0f3.exe?_ga=2.69975952.1053985475.1515441432-1315763065.1501872160"
+$WSADotNetSupportWindowsUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/TargetSupportInstaller/UnitySetup-Metro-Support-for-Editor-2017.3.0f3.exe?_ga=2.69975952.1053985475.1515441432-1315763065.1501872160"
+$WSAIl2CppSupportWindowsUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/TargetSupportInstaller/UnitySetup-UWP-IL2CPP-Support-for-Editor-2017.3.0f3.exe?_ga=2.216707094.1053985475.1515441432-1315763065.1501872160"
 
 #install editor
 $path = (Get-Location).Path + "\UnitySetup64.exe";
