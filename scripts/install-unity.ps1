90f947589a0cf0e6bbc2f5155b85dd595fa2a175
Alexander Chocron <alchocro@microsoft.com>
2018-01-16 10:26:06 -0800

    Change install path

--- scripts/install-unity.ps1
+++ scripts/install-unity.ps1
@@ -1,16 +1,18 @@
 $WindowsUnityEditorUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/Windows64EditorInstaller/UnitySetup64-2017.3.0f3.exe?_ga=2.69975952.1053985475.1515441432-1315763065.1501872160"
 $WSADotNetSupportWindowsUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/TargetSupportInstaller/UnitySetup-Metro-Support-for-Editor-2017.3.0f3.exe?_ga=2.69975952.1053985475.1515441432-1315763065.1501872160";
 
+$InstallPath = (Get-Location).Path + "\Unity"
+
 #install editor
 $path = (Get-Location).Path + "\UnitySetup64.exe";
 Write-Host "Downloading Unity Editor..."
 (New-Object System.Net.WebClient).DownloadFile($WindowsUnityEditorUrl, $path) 
 Write-Host "Installing Unity Editor..."
-./UnitySetup64.exe /S /D=Unity
+./UnitySetup64.exe /S /D=$InstallPath
 
 #install support (.net)
 $path = (Get-Location).Path + "\UnityNetSupport.exe";
 Write-Host "Downloading Unity .NET support..."
 (New-Object System.Net.WebClient).DownloadFile($WSADotNetSupportWindowsUrl, $path) 
 Write-Host "Installing Unity .NET support..."
-./UnityNetSupport.exe /S /D=Unity
+./UnityNetSupport.exe /S /D=$InstallPath
