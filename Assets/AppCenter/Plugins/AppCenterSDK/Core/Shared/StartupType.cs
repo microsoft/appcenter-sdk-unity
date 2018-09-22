78b6471dee67d21b528bdcafed5686df62d1d165
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-09-22 15:30:10 +0300

    Startup type

--- Assets/AppCenter/Plugins/AppCenterSDK/Core/Shared/StartupType.cs
+++ Assets/AppCenter/Plugins/AppCenterSDK/Core/Shared/StartupType.cs
@@ -1,10 +1,27 @@
-﻿using System;
-namespace Application
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the MIT license.
+
+namespace Microsoft.AppCenter.Unity
 {
-    public class NewClass
+    /// <summary>
+    /// Log level threshold for logs emitted by the SDK.
+    /// </summary>
+    public enum StartupType
     {
-        public NewClass()
-        {
-        }
+        /// <summary>
+        /// SDK is turned on automatically.
+        /// </summary>
+        AppCenter = 2,
+
+        /// <summary>
+        /// Analytics service is started from library.
+        /// </summary>
+        OneCollector = 3,
+
+        /// <summary>
+        /// Nothing is configured on startup.
+        /// </summary>
+        None = 4
     }
 }
