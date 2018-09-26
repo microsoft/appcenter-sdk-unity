08e7834b92ca7b093a7551f4803c0c08506b5336
Evgeniy Poletaikin <v-evpole@microsoft.com>
2018-09-26 16:39:47 +0300

    update unity version

--- scripts/install-unity.ps1
+++ scripts/install-unity.ps1
@@ -6,10 +6,10 @@ param(
 
 Write-Host "Installing Unity Editor and tools to directory $InstallPath"
 
-# Download links for Unity 2017.3.0f3/p1, editor and support
-$WindowsUnityEditorUrl = "https://beta.unity3d.com/download/4596dd67072f/Windows64EditorInstaller/UnitySetup64-2017.3.0p1.exe?_ga=2.9321589.1512318840.1516132168-1315763065.1501872160"
-$WSADotNetSupportWindowsUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/TargetSupportInstaller/UnitySetup-Metro-Support-for-Editor-2017.3.0f3.exe?_ga=2.69975952.1053985475.1515441432-1315763065.1501872160"
-$WSAIl2CppSupportWindowsUrl = "https://download.unity3d.com/download_unity/a9f86dcd79df/TargetSupportInstaller/UnitySetup-UWP-IL2CPP-Support-for-Editor-2017.3.0f3.exe?_ga=2.216707094.1053985475.1515441432-1315763065.1501872160"
+# Download links for Unity editor and support
+$WindowsUnityEditorUrl = "https://netstorage.unity3d.com/unity/2207421190e9/Windows64EditorInstaller/UnitySetup64-2018.2.9f1.exe"
+$WSADotNetSupportWindowsUrl = "https://netstorage.unity3d.com/unity/2207421190e9/TargetSupportInstaller/UnitySetup-UWP-.NET-Support-for-Editor-2018.2.9f1.exe"
+$WSAIl2CppSupportWindowsUrl = "https://netstorage.unity3d.com/unity/2207421190e9/TargetSupportInstaller/UnitySetup-UWP-IL2CPP-Support-for-Editor-2018.2.9f1.exe"
 
 #install editor
 $path = (Get-Location).Path + "\UnitySetup64.exe";
