d8a7eda8f9321ed35b6430637abe6c97807e0b1d
Evgeniy Poletaikin <v-evpole@microsoft.com>
2018-09-27 12:36:29 +0300

    do not use InstallPath parameter when installing Unity

--- scripts/install-unity.ps1
+++ scripts/install-unity.ps1
@@ -1,33 +1,29 @@
 # Installs Unity and required components for WSA .NET and IL2CPP builds
 
-param(
-    [string]$InstallPath
-)
-
-Write-Host "Installing Unity Editor and tools to directory $InstallPath"
+Write-Host "Installing Unity Editor and tools"
 
 # Download links for Unity editor and support
 $WindowsUnityEditorUrl = "https://netstorage.unity3d.com/unity/2207421190e9/Windows64EditorInstaller/UnitySetup64-2018.2.9f1.exe"
 $WSADotNetSupportWindowsUrl = "https://netstorage.unity3d.com/unity/2207421190e9/TargetSupportInstaller/UnitySetup-UWP-.NET-Support-for-Editor-2018.2.9f1.exe"
 $WSAIl2CppSupportWindowsUrl = "https://netstorage.unity3d.com/unity/2207421190e9/TargetSupportInstaller/UnitySetup-UWP-IL2CPP-Support-for-Editor-2018.2.9f1.exe"
 
-#install editor
+# Install editor
 $path = (Get-Location).Path + "\UnitySetup64.exe";
 Write-Host "Downloading Unity Editor..."
 (New-Object System.Net.WebClient).DownloadFile($WindowsUnityEditorUrl, $path) 
 Write-Host "Installing Unity Editor..."
-./UnitySetup64.exe /S /D=$InstallPath
+./UnitySetup64.exe /S
 
 # Install support tools (.NET)
 $path = (Get-Location).Path + "\UnityNetSupport.exe";
 Write-Host "Downloading Unity .NET support..."
 (New-Object System.Net.WebClient).DownloadFile($WSADotNetSupportWindowsUrl, $path) 
 Write-Host "Installing Unity .NET support..."
-./UnityNetSupport.exe /S /D=$InstallPath
+./UnityNetSupport.exe /S
 
-#install support tools (IL2CPP)
+# Install support tools (IL2CPP)
 $path = (Get-Location).Path + "\UnityIl2CppSupport.exe";
 Write-Host "Downloading Unity IL2CPP support..."
 (New-Object System.Net.WebClient).DownloadFile($WSAIl2CppSupportWindowsUrl, $path) 
 Write-Host "Installing Unity IL2CPP support..."
-./UnityIl2CppSupport.exe /S /D=$InstallPath
+./UnityIl2CppSupport.exe /S
