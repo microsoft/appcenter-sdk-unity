401bbbec6dc2b01122256587780e33e087ed4c24
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-10-11 10:44:24 +0300

    Demo app (#140)
    * Fix
    
    * To last
    
    * Demo app

--- AppCenterDemoApp/Assets/Demo/DemoAppCenter.cs
+++ AppCenterDemoApp/Assets/Demo/DemoAppCenter.cs
@@ -6,11 +6,100 @@ using System.Collections;
 using Microsoft.AppCenter.Unity;
 using UnityEngine;
 using UnityEngine.UI;
+using Microsoft.AppCenter.Unity.Crashes;
+using Microsoft.AppCenter.Unity.Distribute;
+using Microsoft.AppCenter.Unity.Push;
+using AOT;
 
 public class DemoAppCenter : MonoBehaviour
 {
     public Toggle Enabled;
+    public Text InstallIdLabel;
+    public Text AppSecretLabel;
+    public Text LogUrlLabel;
+    public Text DeviceIdLabel;
+    public Text SdkVersionLabel;
     public Dropdown LogLevel;
+    public PuppetConfirmationDialog userConfirmationDialog;
+    public const string TextAttachmentKey = "text_attachment";
+    public const string BinaryAttachmentKey = "binary_attachment";
+
+    static DemoAppCenter instance;
+
+    public GameObject CustomProperty;
+    public RectTransform PropertiesList;
+    public Toggle DistributeEnabled;
+    public Toggle PushEnabled;
+
+    public void SetPushEnabled(bool enabled)
+    {
+        StartCoroutine(SetPushEnabledCoroutine(enabled));
+    }
+
+    private IEnumerator SetPushEnabledCoroutine(bool enabled)
+    {
+        yield return Push.SetEnabledAsync(enabled);
+        var isEnabled = Push.IsEnabledAsync();
+        yield return isEnabled;
+        PushEnabled.isOn = isEnabled.Result;
+    }
+
+    public void SetDistributeEnabled(bool enabled)
+    {
+        StartCoroutine(SetDistributeEnabledCoroutine(enabled));
+    }
+
+    private IEnumerator SetDistributeEnabledCoroutine(bool enabled)
+    {
+        yield return Distribute.SetEnabledAsync(enabled);
+        var isEnabled = Distribute.IsEnabledAsync();
+        yield return isEnabled;
+        DistributeEnabled.isOn = isEnabled.Result;
+    }
+
+    public void AddProperty()
+    {
+        var property = Instantiate(CustomProperty);
+        property.transform.SetParent(PropertiesList, false);
+    }
+
+    public void Send()
+    {
+        AppCenter.SetCustomProperties(GetProperties());
+    }
+
+    private CustomProperties GetProperties()
+    {
+        var properties = PropertiesList.GetComponentsInChildren<PuppetCustomProperty>();
+        if (properties == null || properties.Length == 0)
+        {
+            return null;
+        }
+        var result = new CustomProperties();
+        foreach (var i in properties)
+        {
+            i.Set(result);
+        }
+        return result;
+    }
+
+    private void Awake()
+    {
+        Crashes.ShouldProcessErrorReport = PuppetCrashes.ShouldProcessErrorReportHandler;
+        Crashes.ShouldAwaitUserConfirmation = UserConfirmationHandler;
+        Crashes.GetErrorAttachments = PuppetCrashes.GetErrorAttachmentstHandler;
+        Crashes.SendingErrorReport += PuppetCrashes.SendingErrorReportHandler;
+        Crashes.SentErrorReport += PuppetCrashes.SentErrorReportHandler;
+        Crashes.FailedToSendErrorReport += PuppetCrashes.FailedToSendErrorReportHandler;
+        instance = this;
+    }
+
+    [MonoPInvokeCallback(typeof(Crashes.UserConfirmationHandler))]
+    public static bool UserConfirmationHandler()
+    {
+        instance.userConfirmationDialog.Show();
+        return true;
+    }
 
     void OnEnable()
     {
@@ -22,10 +111,30 @@ public class DemoAppCenter : MonoBehaviour
         {
             if (task.Result.HasValue)
             {
-                print("Install ID = " + task.Result.ToString());
+                InstallIdLabel.text = task.Result.ToString();
             }
         });
+        AppCenter.GetSecretForPlatform().ContinueWith(task =>
+        {
+            AppSecretLabel.text = task.Result;
+        });
+        AppCenter.GetLogUrl().ContinueWith(task =>
+        {
+            LogUrlLabel.text = task.Result;
+        });
+        DeviceIdLabel.text = SystemInfo.deviceUniqueIdentifier;
+        SdkVersionLabel.text = AppCenter.GetSdkVersion();
         LogLevel.value = AppCenter.LogLevel - Microsoft.AppCenter.Unity.LogLevel.Verbose;
+        Push.IsEnabledAsync().ContinueWith(task =>
+        {
+            PushEnabled.isOn = task.Result;
+        });
+        Distribute.IsEnabledAsync().ContinueWith(task =>
+        {
+            DistributeEnabled.isOn = task.Result;
+        });
+
+        Distribute.ReleaseAvailable = (releaseDetails) => true;
     }
 
     public void SetEnabled(bool enabled)
