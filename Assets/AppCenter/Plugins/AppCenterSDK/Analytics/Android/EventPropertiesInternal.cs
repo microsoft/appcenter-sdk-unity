3abc4c0bd89e0c35ad1c432510e8688db371a01e
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-10-29 13:33:31 +0300

    EventProperties

---
+++ Assets/AppCenter/Plugins/AppCenterSDK/Analytics/Android/EventPropertiesInternal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using Microsoft.AppCenter.Unity.Internal.Utility;
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+namespace Microsoft.AppCenter.Unity.Analytics.Internal
+{
+    class EventPropertiesInternal
+    {
+        public static AndroidJavaObject Create()
+        {
+            return new AndroidJavaObject("com.microsoft.appcenter.analytics.EventProperties");
+        }
+
+        public static void SetString(AndroidJavaObject properties, string key, string val)
+        {
+            properties.Call<AndroidJavaObject>("set", key, val);
+        }
+
+        public static void SetNumber(AndroidJavaObject properties, string key, long val)
+        {
+            properties.Call<AndroidJavaObject>("set", key, JavaNumberHelper.Convert(val));
+        }
+
+        public static void SetNumber(AndroidJavaObject properties, string key, double val)
+        {
+            properties.Call<AndroidJavaObject>("set", key, JavaNumberHelper.Convert(val));
+        }
+
+        public static void SetBool(AndroidJavaObject properties, string key, bool val)
+        {
+            properties.Call<AndroidJavaObject>("set", key, val);
+        }
+
+        public static void SetDate(AndroidJavaObject properties, string key, DateTime val)
+        {
+            properties.Call<AndroidJavaObject>("set", key, JavaDateHelper.DateTimeConvert(val));
+        }
+    }
+}
+#endif
---
+++ Assets/AppCenter/Plugins/AppCenterSDK/Analytics/iOS/EventPropertiesInternal.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+//
+// Licensed under the MIT license.
+
+#if UNITY_IOS && !UNITY_EDITOR
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.AppCenter.Unity.Internal.Utility;
+
+namespace Microsoft.AppCenter.Unity.Analytics.Internal
+{
+    class EventPropertiesInternal
+    {
+        public static IntPtr Create()
+        {
+        }
+
+        public static void SetString(IntPtr properties, string key, string val)
+        {
+        }
+
+        public static void SetNumber(IntPtr properties, string key, long val)
+        {
+        }
+
+        public static void SetNumber(IntPtr properties, string key, double val)
+        {
+        }
+
+        public static void SetBool(IntPtr properties, string key, bool val)
+        {
+        }
+
+        public static void SetDate(IntPtr properties, string key, DateTime val)
+        {
+        }
+
+#region External
+#endregion
+    }
+}
+#endif
