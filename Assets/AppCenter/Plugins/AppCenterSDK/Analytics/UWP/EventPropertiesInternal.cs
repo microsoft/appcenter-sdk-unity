a664595fd63166b8ae250e037e049c45487195f7
Anna Kocheshkova <v-annkoc@microsoft.com>
2018-10-29 16:11:16 +0300

    + UWP

--- Assets/AppCenter/Plugins/AppCenterSDK/Analytics/UWP/EventPropertiesInternal.cs
+++ Assets/AppCenter/Plugins/AppCenterSDK/Analytics/UWP/EventPropertiesInternal.cs
@@ -1,37 +1,49 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 //
 // Licensed under the MIT license.
-
 #if UNITY_WSA_10_0 && !UNITY_EDITOR
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Microsoft.AppCenter.Unity.Analytics.Internal
 {
     class EventPropertiesInternal
     {
-       public static object Create()
+        private static Dictionary<string, string> _properties;
+
+        public static Dictionary<string, string> Create()
         {
-            return null;
+            if (_properties == null) 
+            {
+                _properties = new Dictionary<string, string>();
+            }
+            return _properties;
         }
 
-        public static void SetString(object properties, string key, string val)
+        public static void SetString(Dictionary<string, string> properties, string key, string val)
         {
+            properties[key] = val;
         }
 
-        public static void SetNumber(object properties, string key, long val)
+        public static void SetNumber(Dictionary<string, string> properties, string key, long val)
         {
+            properties[key] = val.ToString();
         }
 
-        public static void SetNumber(object properties, string key, double val)
+        public static void SetNumber(Dictionary<string, string> properties, string key, double val)
         {
+            properties[key] = val.ToString();
         }
 
-        public static void SetBool(object properties, string key, bool val)
+        public static void SetBool(Dictionary<string, string> properties, string key, bool val)
         {
+            properties[key] = val.ToString();
         }
 
-        public static void SetDate(object properties, string key, DateTime val)
+        public static void SetDate(Dictionary<string, string> properties, string key, DateTime val)
         {
+            properties[key] = val.ToLongDateString();
         }
     }
 }
