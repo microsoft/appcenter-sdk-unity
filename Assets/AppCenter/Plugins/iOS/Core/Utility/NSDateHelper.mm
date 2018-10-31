dc39b311be4ad9916d12cc2db1fbd1cce656c8dc
alex-oreshko <alexander.oreshko@akvelon.com>
2018-10-31 16:10:27 +0300

    syntax error fixed

--- Assets/AppCenter/Plugins/iOS/Core/Utility/NSDateHelper.mm
+++ Assets/AppCenter/Plugins/iOS/Core/Utility/NSDateHelper.mm
@@ -7,5 +7,5 @@
 
 void* appcenter_unity_ns_date_convert(long interval)
 {
-    return (void *)CFBridgingRetain([[NSDate alloc] initWithTimeIntervalSince1970:interval);
+    return (void *)CFBridgingRetain([[NSDate alloc] initWithTimeIntervalSince1970:interval]);
 }
