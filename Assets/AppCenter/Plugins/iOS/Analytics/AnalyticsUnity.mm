5bee903db4e765b588cc6124219a3d26debb84db
alex-oreshko <alexander.oreshko@akvelon.com>
2018-11-14 15:07:27 +0300

    trackEvent with flags is not released in native SDK

--- Assets/AppCenter/Plugins/iOS/Analytics/AnalyticsUnity.mm
+++ Assets/AppCenter/Plugins/iOS/Analytics/AnalyticsUnity.mm
@@ -15,19 +15,24 @@ void* appcenter_unity_analytics_get_type()
 
 void appcenter_unity_analytics_track_event(char* eventName, int flags)
 {
-  [MSAnalytics trackEvent:[NSString stringWithUTF8String:eventName] withProperties:NULL flags:flags];
+  [MSAnalytics trackEvent:[NSString stringWithUTF8String:eventName];
+  // Native SKD method with flags is not ready yet
+  //[MSAnalytics trackEvent:[NSString stringWithUTF8String:eventName] withProperties:NULL flags:flags];
 }
 
 void appcenter_unity_analytics_track_event_with_properties(char* eventName, char** keys, char** values, int count, int flags)
 {
   NSDictionary<NSString*, NSString*> *properties = appcenter_unity_create_ns_string_dictionary(keys, values, count);
-  NSString * eventNameString = [NSString stringWithUTF8String:eventName];
-  [MSAnalytics trackEvent:eventNameString withProperties:properties flags:flags];
+  [MSAnalytics trackEvent:[NSString stringWithUTF8String:eventName] withProperties:properties];
+  // Native SKD method with flags is not ready yet
+  //[MSAnalytics trackEvent:[NSString stringWithUTF8String:eventName] withProperties:properties flags:flags];
 }
 
 void appcenter_unity_analytics_track_event_with_typed_properties(char* eventName, MSEventProperties* properties, int flags)
 {
-  [MSAnalytics trackEvent:[NSString stringWithUTF8String:eventName] withTypedProperties:properties flags:flags];
+  [MSAnalytics trackEvent:[NSString stringWithUTF8String:eventName] withTypedProperties:properties];
+  // Native SKD method with flags is not ready yet
+  //[MSAnalytics trackEvent:[NSString stringWithUTF8String:eventName] withTypedProperties:properties flags:flags];
 }
 
 void appcenter_unity_analytics_set_enabled(bool isEnabled)
