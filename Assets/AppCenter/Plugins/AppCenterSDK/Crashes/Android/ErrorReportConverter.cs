7a90833cd294380179ebbcb10ae5091bae128ea9
Evgenii Poletaikin <v-evpole@microsoft.com>
2018-12-18 14:36:59 +0300

    add ErrorReportConverter stub

---
+++ Assets/AppCenter/Plugins/AppCenterSDK/Crashes/Android/ErrorReportConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AppCenter.Unity.Crashes;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErrorReportConverter
+{
+    public static ErrorReport Convert(AndroidJavaObject errorReport)
+    {
+        var report = new ErrorReport(
+            "id",
+            DateTimeOffset.UtcNow,
+            DateTimeOffset.UtcNow,
+            new Microsoft.AppCenter.Unity.Crashes.Models.Exception("condition", "stack trace"),
+            1,
+            "reporter key",
+            "reporter signal",
+            true);
+        return report;
+    }
+}
