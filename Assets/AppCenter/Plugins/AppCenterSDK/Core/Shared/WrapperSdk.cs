a877a5012e349db20af698dd929d7082f23c7e45
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-02-28 17:03:05 +0300

    Release 1.4.0

--- Assets/AppCenter/Plugins/AppCenterSDK/Core/Shared/WrapperSdk.cs
+++ Assets/AppCenter/Plugins/AppCenterSDK/Core/Shared/WrapperSdk.cs
@@ -13,7 +13,7 @@ namespace Microsoft.AppCenter.Unity
         private static bool _hasAttemptedToGetRuntimeVersion;
 
         public const string Name = "appcenter.unity";
-        public const string WrapperSdkVersion = "1.3.0";
+        public const string WrapperSdkVersion = "1.4.0";
 
         internal static string WrapperRuntimeVersion
         {
--- RELEASE.md
+++ RELEASE.md
@@ -1,3 +1,7 @@
+# Release 1.4.0
+
+* Updated native SDK versions.
+
 # Release 1.3.0
 
 **Breaking changes**
