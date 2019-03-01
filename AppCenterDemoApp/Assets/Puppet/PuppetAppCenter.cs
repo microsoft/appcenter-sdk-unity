4a9a99db49ffad1e28f8872a64cee0a8e0f5c751
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-03-01 14:40:32 +0300

    Update demo app

--- AppCenterDemoApp/Assets/Puppet/PuppetAppCenter.cs
+++ AppCenterDemoApp/Assets/Puppet/PuppetAppCenter.cs
@@ -2,18 +2,17 @@
 //
 // Licensed under the MIT license.
 
-using System;
-using System.Collections;
 using Microsoft.AppCenter.Unity;
-using UnityEngine;
-using UnityEngine.UI;
-using Microsoft.AppCenter.Unity.Crashes;
 using Microsoft.AppCenter.Unity.Distribute;
 using Microsoft.AppCenter.Unity.Push;
-using AOT;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class PuppetAppCenter : MonoBehaviour
 {
+    public static string TextAttachmentCached = "";
+    public static string BinaryAttachmentCached = "";
     public Toggle Enabled;
     public Text StorageSizeLabel;
     public Text InstallIdLabel;
@@ -27,9 +26,6 @@ public class PuppetAppCenter : MonoBehaviour
     public const string TextAttachmentKey = "text_attachment";
     public const string BinaryAttachmentKey = "binary_attachment";
     public const string UserIdKey = "user_id";
-
-    static PuppetAppCenter instance;
-
     public GameObject CustomProperty;
     public RectTransform PropertiesList;
     public Toggle DistributeEnabled;
@@ -92,63 +88,62 @@ public class PuppetAppCenter : MonoBehaviour
     private void Awake()
     {
         var customUserId = PlayerPrefs.GetString(UserIdKey);
-        if (customUserId != null && customUserId.Length > 0) 
+        if (customUserId != null && customUserId.Length > 0)
         {
             _customUserId = customUserId;
             AppCenter.SetUserId(customUserId);
         }
-        Crashes.ShouldProcessErrorReport = PuppetCrashes.ShouldProcessErrorReportHandler;
-        Crashes.ShouldAwaitUserConfirmation = UserConfirmationHandler;
-        Crashes.GetErrorAttachments = PuppetCrashes.GetErrorAttachmentstHandler;
-        Crashes.SendingErrorReport += PuppetCrashes.SendingErrorReportHandler;
-        Crashes.SentErrorReport += PuppetCrashes.SentErrorReportHandler;
-        Crashes.FailedToSendErrorReport += PuppetCrashes.FailedToSendErrorReportHandler;
-        instance = this;
-    }
 
-    [MonoPInvokeCallback(typeof(Crashes.UserConfirmationHandler))]
-    public static bool UserConfirmationHandler()
-    {
-        instance.userConfirmationDialog.Show();
-        return true;
+        // Caching this in Awake method because PlayerPrefs.GetString() can't be called from a background thread.
+        TextAttachmentCached = PlayerPrefs.GetString(TextAttachmentKey);
+        BinaryAttachmentCached = PlayerPrefs.GetString(BinaryAttachmentKey);
     }
 
     void OnEnable()
     {
-        AppCenter.IsEnabledAsync().ContinueWith(task =>
+        StartCoroutine(OnEnableCoroutine());
+        StartCoroutine(ShowCustomLogUrl());
+    }
+
+    private IEnumerator ShowCustomLogUrl()
+    {
+        var logUrl = AppCenter.GetLogUrl();
+        yield return logUrl;
+        LogUrlLabel.text = logUrl.Result;
+    }
+
+    private IEnumerator OnEnableCoroutine()
+    {
+        var isEnabled = AppCenter.IsEnabledAsync();
+        yield return isEnabled;
+        Enabled.isOn = isEnabled.Result;
+
+        var installId = AppCenter.GetInstallIdAsync();
+        yield return installId;
+        if (installId.Result.HasValue)
         {
-            Enabled.isOn = task.Result;
-        });
-        AppCenter.GetInstallIdAsync().ContinueWith(task =>
-        {
-            if (task.Result.HasValue)
-            {
-                InstallIdLabel.text = task.Result.ToString();
-            }
-        });
-        AppCenter.GetSecretForPlatform().ContinueWith(task =>
-        {
-            AppSecretLabel.text = task.Result;
-        });
-        AppCenter.GetLogUrl().ContinueWith(task =>
-        {
-            LogUrlLabel.text = task.Result;
-        });
-        AppCenter.GetStorageSize().ContinueWith(task =>
-        {
-            StorageSizeLabel.text = task.Result <= 0 ? "Unchanged" : task.Result.ToString() + " bytes";
-        });
+            InstallIdLabel.text = installId.Result.ToString();
+        }
+
+        var appSecret = AppCenter.GetSecretForPlatform();
+        yield return appSecret;
+        AppSecretLabel.text = appSecret.Result;
+
+        var storageSize = AppCenter.GetStorageSize();
+        yield return storageSize;
+        StorageSizeLabel.text = storageSize.Result <= 0 ? "Unchanged" : storageSize.Result.ToString() + " bytes";
+
         DeviceIdLabel.text = SystemInfo.deviceUniqueIdentifier;
         SdkVersionLabel.text = AppCenter.GetSdkVersion();
         LogLevel.value = AppCenter.LogLevel - Microsoft.AppCenter.Unity.LogLevel.Verbose;
-        Push.IsEnabledAsync().ContinueWith(task =>
-        {
-            PushEnabled.isOn = task.Result;
-        });
-        Distribute.IsEnabledAsync().ContinueWith(task =>
-        {
-            DistributeEnabled.isOn = task.Result;
-        });
+
+        var isPushEnabled = Push.IsEnabledAsync();
+        yield return isPushEnabled;
+        PushEnabled.isOn = isPushEnabled.Result;
+
+        var isDistributeEnabled = Distribute.IsEnabledAsync();
+        yield return isDistributeEnabled;
+        DistributeEnabled.isOn = isDistributeEnabled.Result;
 
         Distribute.ReleaseAvailable = (releaseDetails) => true;
         UserId.text = _customUserId;
