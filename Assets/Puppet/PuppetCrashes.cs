69f07ed39f56ce0c5ddac676ee55bf0025c0ece4
Anastasia Kubova <anastasia.kubova@akvelon.com>
2019-07-10 15:03:33 +0300

    roll back changes and edit ui

--- Assets/Puppet/PuppetCrashes.cs
+++ Assets/Puppet/PuppetCrashes.cs
@@ -49,7 +49,7 @@ public class PuppetCrashes : MonoBehaviour
 
     public void SetCrashesEnabled(bool enabled)
     {
-        StartCoroutine(SetCrashesEnabledCoroutine(CrashesEnabled.isOn));
+        StartCoroutine(SetCrashesEnabledCoroutine(enabled));
     }
 
     public void SetReportUnhandledExceptions(bool enabled)
