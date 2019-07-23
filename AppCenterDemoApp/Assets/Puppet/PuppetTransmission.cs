48d63a3b8ced701e1e090c7b0b834cfd1ddefc3c
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-07-23 13:57:43 +0300

    Added missing import :poop:

--- AppCenterDemoApp/Assets/Puppet/PuppetTransmission.cs
+++ AppCenterDemoApp/Assets/Puppet/PuppetTransmission.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AppCenter.Unity;
 using Microsoft.AppCenter.Unity.Analytics;
 using UnityEngine;
 using UnityEngine.UI;
