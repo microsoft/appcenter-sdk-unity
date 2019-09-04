0bba1697c989184081e698ef9ae9577de4e32078
Amanda Cheng <t-yuqche@microsoft.com>
2019-09-04 13:44:14 -0700

    format

--- Assets/AppCenter/Plugins/iOS/Auth/UserInformation.mm
+++ Assets/AppCenter/Plugins/iOS/Auth/UserInformation.mm
@@ -5,20 +5,17 @@
 #import "UserInformation.h"
 #import <AppCenterAuth/AppCenterAuth.h>
 
-const char* app_center_unity_auth_user_information_account_id(void* userInformation)
-{
+const char* app_center_unity_auth_user_information_account_id(void* userInformation) {
   MSUserInformation *information = (__bridge MSUserInformation*)userInformation;
   return appcenter_unity_ns_string_to_cstr([information accountId]);
 }
 
-const char* app_center_unity_auth_user_information_access_token(void* userInformation)
-{
+const char* app_center_unity_auth_user_information_access_token(void* userInformation) {
   MSUserInformation *information = (__bridge MSUserInformation*)userInformation;
   return appcenter_unity_ns_string_to_cstr([information accessToken]);
 }
 
-const char* app_center_unity_auth_user_information_id_token(void* userInformation)
-{
+const char* app_center_unity_auth_user_information_id_token(void* userInformation) {
   MSUserInformation *information = (__bridge MSUserInformation*)userInformation;
   return appcenter_unity_ns_string_to_cstr([information idToken]);
 }
