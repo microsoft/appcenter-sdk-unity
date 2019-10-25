de707d9cb0955c9926140f670627b52f4ba729f8
Jae Lim <jaelim@microsoft.com>
2019-10-25 15:30:08 -0700

    Remove FilePickerManager

--- Assets/Puppet/FilePicker/IFilePicker.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-internal interface IFilePicker
-{
-    void Show();
-    void InitBytes(string path);
-}
