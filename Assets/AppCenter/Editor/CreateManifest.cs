cfd199734916e9d25e70b07577fe14a5d3f8e57a
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-11-05 18:33:11 +0300

    Prevent duplicate elements

--- Assets/AppCenter/Editor/CreateManifest.cs
+++ Assets/AppCenter/Editor/CreateManifest.cs
@@ -143,36 +143,63 @@ public class CreateManifest
             Directory.Delete(loaderFolder, true);
             return;
         }
-
+        bool activityElementIsNew = false;
         var activityElement = applicationElements[0].Elements().FirstOrDefault(element => element.Name.LocalName == "activity");
         if (activityElement == null)
         {
+            activityElementIsNew = true;
             activityElement = new XElement("activity");
         }
+        bool intentElementIsNew = false;
         var intentElement = activityElement.Elements().FirstOrDefault(element => element.Name.LocalName == "intent-filter");
         if (intentElement == null)
         {
+            intentElementIsNew = true;
             intentElement = new XElement("intent-filter");
         }
 
         XNamespace ns = "http://schemas.android.com/apk/res/android";
         activityElement.SetAttributeValue(ns + "name", "com.microsoft.identity.client.BrowserTabActivity");
-        var actionElement = new XElement("action");
+        var actionElement = intentElement.Elements().FirstOrDefault(element => element.Name.LocalName == "intent-filter");
+        bool actionElementIsNew = false;
+        if (actionElement == null)
+        {
+            actionElementIsNew = true;
+            actionElement = new XElement("action");
+        }
         actionElement.SetAttributeValue(ns + "name", "android.intent.action.VIEW");
         var categoryElement1 = new XElement("category");
         categoryElement1.SetAttributeValue(ns + "name", "android.intent.category.DEFAULT");
         var categoryElement2 = new XElement("category");
         categoryElement2.SetAttributeValue(ns + "name", "android.intent.category.BROWSABLE");
-        var dataElement = new XElement("data");
+        bool dataElementIsNew = false;
+        var dataElement = intentElement.Elements().FirstOrDefault(element => element.Name.LocalName == "data");
+        if (dataElement == null)
+        {
+            dataElementIsNew = true;
+            dataElement = new XElement("data");
+        }
         dataElement.SetAttributeValue(ns + "host", "auth");
         dataElement.SetAttributeValue(ns + "scheme", "msal" + settings.AndroidAppSecret);
 
-        intentElement.Add(actionElement);
+        if (actionElementIsNew)
+        {
+            intentElement.Add(actionElement);
+        }
         intentElement.Add(categoryElement1);
         intentElement.Add(categoryElement2);
-        intentElement.Add(dataElement);
-        activityElement.Add(intentElement);
-        applicationElements[0].Add(activityElement);
+        if (dataElementIsNew)
+        {
+            intentElement.Add(dataElement);
+        }
+        if (intentElementIsNew)
+        {
+            activityElement.Add(intentElement);
+        }
+        if (activityElementIsNew)
+        {
+            applicationElements[0].Add(activityElement);
+        }
         xmlFile.Save(manifestPath);
 
         // Delete the AndroidManifest.xml.meta file if generated
